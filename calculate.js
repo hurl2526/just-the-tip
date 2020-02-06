9b8586e0c4528be35d7fdfd41069492a6c40c915
Patrick <patrick.hurley@codeimmersives.com>
2020-02-06 13:17:22 -0500

    trying different stuff

--- calculate.js
+++ calculate.js
@@ -1,14 +1,36 @@
 
 
-function calculate(billAmount,tipPercent,amountOfPeople){
+function calculate(billAmount,tipDescription,amountOfPeople){
+  if (tipDescription === 'great' 
+  || tipDescription === "excellent" 
+  || tipDescription === 'awesome' 
+  || tipDescription === 'amazing'
+  || tipDescription === 'spectacular'
+  || tipDescription === 'terrific') {
+    tipDescription = 20 / 100
+  }else if(tipDescription === 'good' 
+  || tipDescription === "ok" 
+  || tipDescription === 'decent' 
+  || tipDescription === 'average'
+  || tipDescription === 'mediocre'
+  || tipDescription === 'content'
+  || tipDescription === 'subpar') {
+    tipDescription = 15 / 100
+  }else if(tipDescription === 'bad' 
+  || tipDescription === "poor" 
+  || tipDescription === 'shit' 
+  || tipDescription === 'awful'
+  || tipDescription === 'crap'
+  || tipDescription === 'terrible') {
+    tipDescription = 5 / 100}
+    else if (tipDescription !== NaN)
+    tipDescription = Number(tipDescription)
+    else {tipDescription= 20 / 100}
+    
   billAmount = Number(billAmount)
-  tipPercent = Number(tipPercent) / 100
   amountOfPeople = Number(amountOfPeople)
-  return billAmount * tipPercent / amountOfPeople
-}
-
-let answer = calculate(50,20,5)
-answer;
+return billAmount * tipDescription / amountOfPeople
+}  
 
 
 if (typeof calculate === 'undefined') {
--- just-the-tip.js
+++ just-the-tip.js
@@ -1,10 +1,9 @@
 const calculate = require('./calculate.js')
 
 let billAmount = process.argv[2]
-let tipPercent= process.argv[3]
+let tipDescription= process.argv[3]
 let amountOfPeople = process.argv[4]
 
 
-const result = calculate(billAmount, tipPercent, amountOfPeople);
-
+const result = calculate(billAmount, tipDescription, amountOfPeople);
 console.log(result);  
